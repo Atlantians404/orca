448631e90b353215390ae776ab980fdfdc6d8b4b
MITHULVS <vsmithul2007@gmail.com>
2026-08-24 12:34:52 +0000

    feat:Add DB connection and main.py

---
+++ backend/database/database.py
@@ -0,0 +1,37 @@
+import os
+
+from dotenv import load_dotenv
+
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncSession,
+    async_sessionmaker
+)
+
+from sqlalchemy.orm import declarative_base
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=False
+)
+
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    autoflush=False,
+    expire_on_commit=False
+)
+
+Base = declarative_base()
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+async def get_db():
+    async with SessionLocal() as db:
+        yield db
