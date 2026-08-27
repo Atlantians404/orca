0d9ceee622ac87e1ce8d5389979196636b434830
Kesavan00725 <kesavansekar25@gmail.com>
2026-08-27 18:19:45 +0530

    feat haversine distance

--- ai/engines/route_engine.py
+++ ai/engines/route_engine.py
@@ -1,3 +1,4 @@
+import math 
 class RouteEngine:
 
     @staticmethod
@@ -17,3 +18,41 @@ class RouteEngine:
             and
             -180 <= longitude <= 180
         )
+
+    
+    @staticmethod
+    def calculate_distance(
+        point_a: tuple[float, float],
+        point_b: tuple[float, float]
+    ) -> float:
+
+        lat1, lon1 = point_a
+        lat2, lon2 = point_b
+
+        if not RouteEngine.validate_coordinates(lat1, lon1):
+            raise ValueError("Invalid coordinates for point A")
+
+        if not RouteEngine.validate_coordinates(lat2, lon2):
+            raise ValueError("Invalid coordinates for point B")
+
+        earth_radius_km = 6371.0
+
+        lat1 = math.radians(lat1)
+        lat2 = math.radians(lat2)
+
+        delta_lat = lat2 - lat1
+        delta_lon = math.radians(lon2 - lon1)
+
+        a = (
+            math.sin(delta_lat / 2) ** 2
+            + math.cos(lat1)
+            * math.cos(lat2)
+            * math.sin(delta_lon / 2) ** 2
+        )
+
+        c = 2 * math.atan2(
+            math.sqrt(a),
+            math.sqrt(1 - a)
+        )
+
+        return earth_radius_km * c
--- test/task4/test_route_engine.py
+++ test/task4/test_route_engine.py
@@ -1,3 +1,4 @@
+import pytest 
 from ai.engines.route_engine import RouteEngine
 
 
@@ -31,3 +32,58 @@ def test_boundary_coordinates():
         90,
         180
     ) is True
+
+def test_same_point_distance():
+
+    distance = RouteEngine.calculate_distance(
+        (13.0827, 80.2707),
+        (13.0827, 80.2707)
+    )
+
+    assert distance == pytest.approx(0, abs=0.001)
+
+def test_distance_is_positive():
+
+    distance = RouteEngine.calculate_distance(
+        (13.0827, 80.2707),
+        (12.9249, 80.1000)
+    )
+
+    assert distance > 0
+
+def test_distance_is_symmetric():
+
+    point_a = (13.0827, 80.2707)
+    point_b = (12.9249, 80.1000)
+
+    distance_ab = RouteEngine.calculate_distance(
+        point_a,
+        point_b
+    )
+
+    distance_ba = RouteEngine.calculate_distance(
+        point_b,
+        point_a
+    )
+
+    assert distance_ab == pytest.approx(
+        distance_ba,
+        abs=0.001
+    )
+
+def test_invalid_point_a():
+
+    with pytest.raises(ValueError):
+
+        RouteEngine.calculate_distance(
+            (100, 80),
+            (13, 80)
+        )
+def test_invalid_point_b():
+
+    with pytest.raises(ValueError):
+
+        RouteEngine.calculate_distance(
+            (13, 80),
+            (100, 80)
+        )
