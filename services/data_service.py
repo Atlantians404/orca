978bdbe22e92bfc69162cb7e9e05454e6fbe817e
Harsheni KG <harshenikamelraaj@gmail.com>
2026-08-30 20:17:37 +0530

    Refactor weather data functions for clarity

--- services/data_service.py
+++ services/data_service.py
@@ -9,7 +9,6 @@ def get_temperature(
     data = get_open_meteo_data(latitude, longitude, time)
     return data["weather"]["temperature_2m"]
 
-
 def get_wind_speed(
     latitude: float,
     longitude: float,
