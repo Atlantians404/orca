805614eddccf9aeebcf0d0a24c839042a1839497
Sadhana <rssadhana0487@gmail.com>
2026-09-12 13:40:33 +0530

    Corrected data engine

--- ai/agents/data_collection_agent/data_collection_engine.py
+++ ai/agents/data_collection_agent/data_collection_engine.py
@@ -49,13 +49,12 @@ def format_data(
             "sea_surface_temperature": marine_data.get(
                 "sea_surface_temperature"
             ),
-            "sea_level_height": marine_data.get(
-                "sea_level_height_msl"
-            ),
-            "marine_warning": (
-                marine_data.get("high_wave_warning")
-                or marine_data.get("swell_surge_warning")
-            )
+            "sea_level_height_msl": marine_data.get(
+    "sea_level_height_msl"
+),
+            "marine_warning": marine_data.get(
+    "warning"
+)
         },
         "weather": {
             "wind_speed": weather_data.get(
