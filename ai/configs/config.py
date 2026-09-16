2bbbd7fba3af08cf7c83da1a2f62833a0d7f1c26
MITHULVS <vsmithul2007@gmail.com>
2026-09-16 15:03:12 +0000

    restore test_graph.py

--- ai/configs/config.py
+++ ai/configs/config.py
@@ -9,7 +9,6 @@ groq_api_key = os.getenv("GROQ_API_KEY")
 llm = ChatGroq(
     model="openai/gpt-oss-20b",
     temperature=0.2,
-    max_tokens=500,
-    api_key=groq_api_key,
-    reasoning_effort="low"
+    max_tokens=1000,
+    api_key=groq_api_key
 )
