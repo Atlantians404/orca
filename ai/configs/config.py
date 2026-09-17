daf991c7c2a38c76deef811cbc3c1f0e0784e6fb
MITHULVS <vsmithul2007@gmail.com>
2026-09-17 07:41:11 +0000

    refactor: weather

--- ai/configs/config.py
+++ ai/configs/config.py
@@ -8,7 +8,7 @@ load_dotenv()
 groq_api_key = os.getenv("GROQ_API_KEY")
 
 llm = ChatGroq(
-    model="openai/gpt-oss-20b",
+    model="qwen/qwen3.8-27b",
     temperature=0,
     max_tokens=800,
     api_key=groq_api_key,
